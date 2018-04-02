6ee273c1c719db057972c9d2d1f62dd07b4a03ea
wujunbo <wujunbo1111@yeah.net>
2018-04-02 10:12:48 +0800

    错误：java.lang.IllegalArgumentException: Request header is too large

---
+++ demo-uploadfile/src/main/java/com/zrkj/DemoUploadfileApplication.java
@@ -0,0 +1,12 @@
+package com.zrkj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoUploadfileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoUploadfileApplication.class, args);
+    }
+}
