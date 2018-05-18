a3c102a97432e373f4bb1f521e0d27cf5a64f4f5
wujunbo <wujunbo1111@yeah.net>
2018-05-18 09:34:35 +0800

    整理电子书籍单独存放

--- java-study/src/main/java/com/zrkj/nio/BufferExample.java
+++ java-study/src/main/java/com/zrkj/nio/BufferExample.java
@@ -18,5 +18,49 @@ public class BufferExample {
         System.out.println("初始时-->mark--->" + byteBuffer.mark());
 
         System.out.println("--------------------------------");
+
+        // 添加数据到缓冲区
+        String s = "Java3y";
+        byteBuffer.put(s.getBytes());
+
+        System.out.println("put完之后-->limit--->" + byteBuffer.limit());
+        System.out.println("put完之后-->position--->" + byteBuffer.position());
+        System.out.println("put完之后-->capacity--->" + byteBuffer.capacity());
+        System.out.println("put完之后-->mark--->" + byteBuffer.mark());
+
+        System.out.println("--------------------------------");
+
+        // 从缓冲区获取数据
+        // flip 切换成读模式
+        byteBuffer.flip();
+
+        System.out.println("flip完之后-->limit--->" + byteBuffer.limit());
+        System.out.println("flip完之后-->position--->" + byteBuffer.position());
+        System.out.println("flip完之后-->capacity--->" + byteBuffer.capacity());
+        System.out.println("flip完之后-->mark--->" + byteBuffer.mark());
+
+        System.out.println("--------------------------------");
+
+        // 切换成读模式就可以读取数据
+        byte[] bytes = new byte[byteBuffer.limit()];
+        // 读取数据到字节数组
+        byteBuffer.get(bytes);
+        // 输出数据
+        System.out.println(new String(bytes, 0, bytes.length));
+
+        System.out.println("get完之后-->limit--->" + byteBuffer.limit());
+        System.out.println("get完之后-->position--->" + byteBuffer.position());
+        System.out.println("get完之后-->capacity--->" + byteBuffer.capacity());
+        System.out.println("get完之后-->mark--->" + byteBuffer.mark());
+
+        System.out.println("--------------------------------");
+
+        // 读完后还想写数据到缓冲区,则调用clear(清空缓冲区)
+        byteBuffer.clear();
+
+        System.out.println("clear完之后-->limit--->" + byteBuffer.limit());
+        System.out.println("clear完之后-->position--->" + byteBuffer.position());
+        System.out.println("clear完之后-->capacity--->" + byteBuffer.capacity());
+        System.out.println("clear完之后-->mark--->" + byteBuffer.mark());
     }
 }
--- java-study/src/main/java/com/zrkj/nio/ChannelExample.java
+++ java-study/src/main/java/com/zrkj/nio/ChannelExample.java
@@ -1,4 +1,25 @@
 package com.zrkj.nio;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.nio.file.Paths;
+
 public class ChannelExample {
+
+    public static void main(String[] args) {
+        try {
+            FileInputStream fileInputStream = new FileInputStream("C:\\work\\2018\\idea\\spring-guides\\notebook\\Java\\NIO.md");
+            // 获取文件输入管道
+            FileChannel fileChannel = fileInputStream.getChannel();
+
+            // 1.7之后
+            FileChannel fileChannel1 = FileChannel.open(Paths.get("C:\\work\\2018\\idea\\spring-guides\\notebook\\Java\\NIO.md"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
