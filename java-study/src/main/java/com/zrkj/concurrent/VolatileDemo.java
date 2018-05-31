b27f056a0a4ae70ca3acb41963f5ff17aa4c9813
wujunbo <wujunbo1111@yeah.net>
2018-05-31 10:30:30 +0800

    并发

---
+++ java-study/src/main/java/com/zrkj/concurrent/VolatileDemo.java
@@ -0,0 +1,16 @@
+package com.zrkj.concurrent;
+
+public class VolatileDemo {
+
+    private int a = 0;
+    private volatile boolean flag = false;
+    public void writer(){
+        a = 1;          //1
+        flag = true;   //2
+    }
+    public void reader(){
+        if(flag){      //3
+            int i = a; //4
+        }
+    }
+}
